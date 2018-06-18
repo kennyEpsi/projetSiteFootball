f5b68b763f8cb7490006aa1f992bea2936e96fcb
DESKTOP-48S0I2K\rober <kenny.robert@epsi.fr>
2018-06-18 10:21:33 +0200

    modif lib

---
+++ src/fr/FootInto/utils/DataConnect.java
@@ -0,0 +1,25 @@
+package fr.FootInto.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataConnect {
+
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+
+		String url = "jdbc:mysql://localhost:3306/football?useSSL=false";
+		String user = "root";
+		String password = "";
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		Connection connection = DriverManager.getConnection(url, user, password);
+		
+		return connection;
+	}
+
+	public static void closeConnection(Connection connection) throws SQLException {
+		connection.close();
+	}
+}
